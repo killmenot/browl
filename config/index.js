0be491cdde1b1e61e1a4262b73481d0fe84db986
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-09-26 23:04:24 +0300

    refactoring plugins

--- config/index.js
+++ config/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const os = require('os');
 const path = require('path');
 const fs = require('fs-extra');
 const ini = require('ini');
 
-const CONFIG_DIR = process.env.CONFIG_DIR || '/etc/browl';
+const CONFIG_DIR = process.env.CONFIG_DIR || path.join(os.homedir(), '.browl');
 const CONFIG_INI = path.join(CONFIG_DIR, 'browl.ini');
-
 const config = ini.parse(fs.readFileSync(CONFIG_INI, 'utf-8'));
+
 config.dir = CONFIG_DIR;
 config.conf_dir = path.join(CONFIG_DIR, 'conf.d');
+config.plugins_dir = path.join(CONFIG_DIR, 'plugins');
 
 config.loadRepoConfig = function (repo) {
   const filename = path.join(this.conf_dir, repo, 'config.ini');
